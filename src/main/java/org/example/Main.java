d4efaade48033afa47c42975640042cdeffc1bf0
Anderson0512 <joao_anderson26@hotmail.com>
2022-12-12 14:16:08 -0300

    Department entity class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
 
+import org.example.model.entities.Department;
+
 public class Main {
     public static void main(String[] args) {
+        Department obj = new Department(1, "'Joao'");
+        System.out.println(obj);
     }
 }
--- src/main/java/org/example/model/entities/Department.java
+++ src/main/java/org/example/model/entities/Department.java
@@ -3,14 +3,20 @@ package org.example.model.entities;
 
 import lombok.*;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
 @ToString
 @EqualsAndHashCode
-public class Departments {
+public class Department implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
     private  Integer id;
     private String name;
 
-    public Departments(Integer id, String name) {
+    public Department(Integer id, String name) {
         this.id = id;
         this.name = name;
     }
